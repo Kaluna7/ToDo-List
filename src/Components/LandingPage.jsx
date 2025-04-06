aca2dedd73dd0ed3859aba72548f714436144e88
Kaluna7 <134622779+Kaluna7@users.noreply.github.com>
2025-04-06 09:47:48 +0800

    UI LandingPage.Jsx component Finished.

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -5,7 +5,8 @@ export default function LandingPage() {
     <div className="bg-[#C4C4C4] min-h-screen min-w-screen flex items-center justify-center">
       <div className="grid grid-cols-2 gap-[150px] justify-items-center items-center">
         <img src={book} className="w-[650px] h-[700px] rounded-4xl ml-[20%]" />
-        <section className="grid grid-rows-3 items-center justify-items-center bg-[#FFF9F9] w-[650px] h-[700px] rounded-4xl ">
+        <section className="bg-[#FFF9F9] w-[650px] h-[700px] rounded-4xl ">
+          <div className='grid grid-rows-[repeat(3,120px)] items-center justify-items-center  mt-[20%]'>
           <h1>ToDo Py</h1>
           <p>
             Stay Organized, Get Things Done: Your
@@ -17,8 +18,9 @@ export default function LandingPage() {
             <br />
             responsibilities.
           </p>
-          <button style={{ backgroundColor: "#76DE37" }} className="px-4 py-2 rounded w-[350px]">Get Started</button>
-
+          <button style={{ backgroundColor: "#76DE37" }} className="px-4 py-2 rounded w-[310px]">Get Started</button>
+          <p>Already have an account? <a className='cursor-pointer'>Sign In</a></p>
+          </div>
         </section>
       </div>
     </div>
