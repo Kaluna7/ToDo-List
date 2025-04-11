f0da2904b36e47f09e475b0b8169b63129057923
Kaluna7 <134622779+Kaluna7@users.noreply.github.com>
2025-04-11 22:29:59 +0800

    Repait UI in UpComing.jsx

--- src/Component/UpComing.jsx
+++ src/Component/UpComing.jsx
@@ -2,14 +2,14 @@ export default function UpComing(){
     return(
         <div className="flex flex-col">
         <h1 style={{fontSize:'45px'}} className="font-bold mb-6">Upcoming</h1>
-        <div className="border w-[700px] h-[320px] rounded-4xl p-6">
+        <div className="border w-[980px] h-[320px] rounded-4xl p-6">
             <h1 style={{fontSize:'32px'}} className="font-bold">Today</h1>
         </div>
         <div className="flex flex-row mt-[45px] gap-[40px]">
-            <div className="border w-[330px] h-[260px] rounded-4xl p-6">
+            <div className="border w-[470px] h-[260px] rounded-4xl p-6">
                 <h1 style={{fontSize:'32px'}} className="font-bold">Tomorrow</h1>
             </div>
-            <div className="border w-[330px] h-[260px] rounded-4xl p-6">
+            <div className="border w-[470px] h-[260px] rounded-4xl p-6">
                 <h1 style={{fontSize:'32px'}} className="font-bold">This Week</h1>
             </div>
         </div>
