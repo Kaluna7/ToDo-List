9c6f25faeb33f9a195665b70af7ae06e2396010e
Kaluna7 <134622779+Kaluna7@users.noreply.github.com>
2025-04-11 22:34:19 +0800

    Work on Today.jsx

--- src/Component/Today.jsx
+++ src/Component/Today.jsx
@@ -1,5 +1,10 @@
 export default function Today(){
     return(
+        <div className="flex flex-col">
         <h1>Today</h1>
+        <div className="border w-[980px] h-[642px] rounded-4xl">
+
+        </div>
+        </div>
     );
 }
