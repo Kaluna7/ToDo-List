917fce11c38e908314ac888f592c04705135b3cc
Kaluna7 <134622779+Kaluna7@users.noreply.github.com>
2025-04-11 23:28:20 +0800

    Today.jsx has finished

--- src/Component/Today.jsx
+++ src/Component/Today.jsx
@@ -1,9 +1,11 @@
 export default function Today(){
     return(
         <div className="flex flex-col">
-        <h1>Today</h1>
-        <div className="border w-[980px] h-[642px] rounded-4xl">
-
+        <h1 className="mb-6">Today</h1>
+        <div className="border w-[980px] h-[618px] rounded-4xl p-6">
+            <div className="">
+                <button style={{background:'#76DE37'}} className="">➕ Add new task</button>
+                </div>
         </div>
         </div>
     );
