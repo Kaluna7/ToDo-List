4973e4db3ce0be9db790d365e61f62daf0ee86a0
Kaluna7 <kaluna12345678910@gmail.com>
2025-04-23 08:14:41 +0800

    Working on AddTask.jsx design the Pop-Up

--- src/Component/NewTask/AddTask.jsx
+++ src/Component/NewTask/AddTask.jsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import axios from "axios";
 
 export default function AddTask({show , onClose}) {
-  const [addTask, setAddTask] = useState("");
+  const [addTask, setAddTask] = useState();
+  const [ description , setDescription ] = useState();
 
   const handleNewTask = async (e) => {
     e.preventDefault();
@@ -12,30 +13,41 @@ export default function AddTask({show , onClose}) {
     try {
       const res = await axios.post("http://localhost:5000/addtask", {
         email,
-        title: addTask,
+        title : addTask,
+        description : description
       });
 
-      console.log("Task berhasil ditambahkan:", res.data);
+      alert("Added Successfully!:", res.data);
       setAddTask("");
     } catch (err) {
-      console.error("Gagal menambahkan task", err);
+      alert("Error Add Task!", err);
     }
   };
 
   if (!show) return null;
   return (
-    <div className="fixed inset-0  bg-opacity-0 flex items-center justify-center">
-      <div className="bg-[#C4C4C4] rounded-lg p-6 w-[600px] h-[400px]">
-      <h2>Tambah To-do</h2>
-      <form onSubmit={handleNewTask}>
+    <div className="fixed bg-opacity-0 flex items-center justify-center ml-50 mt-10">
+      <div className="grid grid-rows-[repeat(2,30px)] bg-[#C4C4C4] rounded-lg p- w-[600px] h-[400px] text-center">
+      <h2 className="font-extrabold text-[32px]">Tambah To-do</h2>
+      <form className="grid grid-rows-[repeat(4,70px)] gap-5 justify-center items-center mt-15" onSubmit={handleNewTask}>
         <input
+          className="border-2"
           type="text"
-          placeholder="Masukkan task"
+          placeholder="Title"
           value={addTask}
           onChange={(e) => setAddTask(e.target.value)}
         />
+          <input
+          className="border-2"
+          type="text"
+          placeholder="Description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <div className="flex gap-30">
         <button type="submit">Tambah</button>
         <button onClick={onClose}>Close</button>
+        </div>
       </form>
       </div>
     </div>
