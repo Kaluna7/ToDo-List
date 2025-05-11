892aa22a93a6ca5bcea46223b20178c14e2ca64c
Kaluna7 <kaluna12345678910@gmail.com>
2025-05-11 12:15:08 +0800

    Work on StickyWall.jsx & Create new utils StickyWallPaper.jsx

--- src/Component/NewTask.jsx
+++ src/Component/NewTask.jsx
@@ -10,10 +10,9 @@ export default function NewTask() {
       <h1 className="mb-6">New Task</h1>
       <div className="border w-[980px] h-[618px] rounded-4xl p-6">
         <div className="">
-          <button style={{ background: "#76DE37" }} className="" onClick={() => {setAddTask(true)}}>
+          <button style={{ background: "#76DE37" }} className="shadow-lg" onClick={() => {setAddTask(true)}}>
             ➕ Add new task
           </button>
-
           <AddTask show={showAddTask} onClose={() => {setAddTask(false)}}/>
         </div>
       </div>
--- src/Component/StickyWall.jsx
+++ src/Component/StickyWall.jsx
@@ -1,3 +1,17 @@
+import StickyWallPaper from "./utils/StickyWallPaper";
+
 export default function StickyWall() {
-  return <h1>Sticky Wall</h1>;
+  return(
+  <div className="flex flex-col">
+    <div className="flex flex-row space-x-140">
+ <h1 className="mb-6">Sticky Wall</h1>
+        <button style={{background : "#76DE37" , hover : "black"}} className="h-15 shadow-lg">New Sticky Wall</button>
+    </div>
+        <div className="border w-[980px] h-[618px] rounded-4xl p-6">
+          <div className="">
+            <StickyWallPaper></StickyWallPaper>
+          </div>
+        </div>
+      </div>
+  );
 }
