33f439777f49d38287fd0525902cbf66f3ca3d58
Kaluna7 <kaluna12345678910@gmail.com>
2025-05-13 21:46:20 +0800

    Repair the pop-up position AddStickyNote.jsx

--- src/Component/NewNote/AddStickyNote.jsx
+++ src/Component/NewNote/AddStickyNote.jsx
@@ -28,12 +28,14 @@ export default function AddStickyNote({show , onClose}) {
     
           if (!show) return null;
     return(
-      <div className="" >
-      <form className="">
-        <input type="text" placeholder="Title" value={addStickyNote} onChange={(e) => setAddStickyNote(e.target.value)} />
-        <input type="text" placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)} />
-        <button onClick={handleAddNote}>Submit</button>
-        <button onClick={onClose}>Exit</button>
+      <div className="fixed bg-blue-200 bg-opacity-0 justify-center ml-80 mt-30" >
+      <form className="grid grid-rows-[repeat(2,50px)] justify-center p-5 gap-5">
+        <input type="text" placeholder="Title" value={addStickyNote} onChange={(e) => setAddStickyNote(e.target.value)} className="border " />
+        <input type="text" placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)} className="border" />
+        <div className="flex gap-30">
+          <button onClick={handleAddNote}>Submit</button>
+          <button onClick={onClose}>Exit</button>
+        </div>
       </form>
       </div>
     );
--- src/Component/StickyWall.jsx
+++ src/Component/StickyWall.jsx
@@ -10,10 +10,10 @@ export default function StickyWall() {
     <div className="flex flex-row space-x-140">
 <h1 className="mb-6">Sticky Wall</h1>
         <button style={{background : "#76DE37"}} className="h-15 shadow-lg" onClick={() => {setShowAddNote(true)}} >New Sticky Wall</button>
-        <AddStickyNote show={showAddNote} onClose={() => {setShowAddNote(false)}} />
     </div>
         <div className="border w-[980px] h-[618px] rounded-4xl p-6">
           <div className="">
+            <AddStickyNote show={showAddNote} onClose={() => {setShowAddNote(false)}} />
             <StickyWallPaper></StickyWallPaper>
           </div>
         </div>
