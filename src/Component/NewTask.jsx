d8cbf720a81c385435e2d9100449255c00a5017b
Kaluna7 <kaluna12345678910@gmail.com>
2025-05-16 23:35:27 +0800

    Design NewTask.jsx

--- src/Component/NewTask.jsx
+++ src/Component/NewTask.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import AddTask from "./NewTask/AddTask";
+import task from "../assets/task.png";
 
 export default function NewTask() {
 
@@ -7,12 +8,15 @@ export default function NewTask() {
   return (
     <div className="flex flex-col">
       <h1 className="mb-6">New Task</h1>
-      <div className="border w-[980px] h-[618px] rounded-4xl p-6">
+      <div className="border w-[980px] h-[618px] rounded-4xl p-6 relative bg-[#B1C9EF]">
         <div className="">
           <button style={{ background: "#76DE37" }} className="shadow-lg" onClick={() => {setAddTask(true)}}>
             ➕ Add new task
           </button>
           <AddTask show={showAddTask} onClose={() => {setAddTask(false)}}/>
+            <div className="bg-[#8AAEE0] w-full h-100 justify-center absolute top-25 right-0 rounded-xl shadow-[10px ,10px,10px,100px] ">
+               <img src={task} id="task" className="w-100 h-100 right-0 absolute"></img>
+            </div>
         </div>
       </div>
     </div>
